d8ad088e40c73b3753ce4cfeda4319eae90e3c88
ahmedRmohamed <ahmedrmohamed2004@gmail.com>
2025-04-28 17:13:56 +0300

    Update footer.component.ts

--- src/app/templete/footer/footer.component.ts
+++ src/app/templete/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faWhatsapp = faWhatsapp;
 }
